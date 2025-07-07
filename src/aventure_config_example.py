362e81c8e586c9b7793757af45c4c294bd7145c4
Mekelaina <truemr.derpster@gmail.com>
2025-07-07 18:21:43 -0400

    added hidden config file and examle for documentation

---
+++ src/aventure_config_example.py
@@ -0,0 +1,5 @@
+
+# An example of the Config.py file that is needed for AventureBot. 
+# to set it up, copy this file as Config.py and put your actual token in here.
+
+TOKEN: str = 'TOTALLY-LEGIT_TOKEN'
