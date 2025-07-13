a303ece32b5fc1609232b55bb791a2309e31343a
Mekelaina <truemr.derpster@gmail.com>
2025-07-13 15:58:08 -0400

    added check for DM or server. added command to send dm

--- src/aventure.py
+++ src/aventure.py
@@ -12,31 +12,60 @@ import aventure_config as config
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='av ', intents=intents)
+bot = commands.Bot(command_prefix=config.COMMAND_PREFIX, intents=intents)
 
 client = discord.Client(intents=intents)
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 
+
+
+# === util functions === #
+
+# checks if a command was sent in a server or not.
+# returns false if guild (server) is None. else true.
+async def is_in_guild(ctx: commands.Context) -> bool:
+    #print(ctx.guild)
+    if ctx.guild == None:
+        return False
+    else:
+        return True
+
+
+# === bot code === #
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    
-    await bot.process_commands(message)
-
+# ping command. really used for debugging. will be removed later
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def ping(ctx: commands.Context):
+    
+    if await is_in_guild(ctx):
+        await ctx.send('Loop')
+    else:
+        await ctx.send('Zoop')
+
+# sends a DM to the user if called from a server. 
+# Since the game part takes place in dms. 
+@bot.command()
+async def dm(ctx: commands.Context):
+    # get user from context
+    user = ctx.author
+    
+    if await is_in_guild(ctx):
+        # checks if the user does not have a dm channel aready
+        # and makes one if needed. should rarely ever be called
+        # according to docs (https://discordpy.readthedocs.io/en/stable/api.html#discord.User.create_dm)
+        if user.dm_channel == None:
+            await user.create_dm()
+        
+        await user.send(f'Hello~ Type \'{config.COMMAND_PREFIX}help\' for getting started!')
+    else:
+        await user.send("We're already in DMs, silly")
 
 
-#bot.add_command(ping2)
+# run the bot
 bot.run(config.TOKEN, log_handler=handler, log_level=logging.DEBUG)
