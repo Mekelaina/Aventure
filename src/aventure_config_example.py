bb148d0eb952bea5812425be13677cdb28017790
Mekelaina <truemr.derpster@gmail.com>
2025-07-18 11:29:45 -0400

    added DB name to config. added comments to example

--- src/aventure_config_example.py
+++ src/aventure_config_example.py
@@ -2,6 +2,12 @@
 # An example of the Config.py file that is needed for AventureBot. 
 # to set it up, copy this file as Config.py and put your actual token in here.
 
+# Set this to your token for your bot instance
 TOKEN: str = 'TOTALLY-LEGIT_TOKEN'
 
+# Set whatever comman prefix you want
+# (spaceing important!)
 COMMAND_PREFIX = 'av '
+
+# Set this to whatever SQLite DB name you wish
+DATABASE = 'test.db'
