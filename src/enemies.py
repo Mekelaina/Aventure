a0505f6b275d54f07f1cff361939c65b5f96c9b6
Mekelaina <truemr.derpster@gmail.com>
2025-07-26 08:56:27 -0400

    maps serialize/deserialize

--- src/enemies.py
+++ src/enemies.py
@@ -2,9 +2,12 @@ from enum import IntEnum
 from aventure_enemy import *
 
 class EnemyID(IntEnum):
-    LARGE_RAT = 0
+    NO_ENEMY = 0 # dummy enemy that isnt actually an enemy. but marks no enemey
+    LARGE_RAT = 1
 
 GAME_ENEMIES: dict[EnemyID : GameEnemy] = {
+    EnemyID.NO_ENEMY : GameEnemy(EnemyID.NO_ENEMY, name='', desc='', ascii='', maxHealth=0,
+                                 atk=0, deff=0, xp=0, gold=0),
     EnemyID.LARGE_RAT : GameEnemy(EnemyID.LARGE_RAT, name='Large Rat', desc= 'A rat the size of a large dog',
                               ascii='R', maxHealth=5, atk=2, deff=0, xp=10, gold=2)
 }
